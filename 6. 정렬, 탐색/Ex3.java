b0d552ff74bbca878912b43c8a0322616a50793e
sun <78401555+AkkeSun@users.noreply.github.com>
2021-03-10 00:05:31 +0900

    Create Ex3.java

---
+++ 6. 정렬, 탐색/Ex3.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Practice {
+	
+	/* 
+	 [부품찾기]
+	 arr1 에서 arr2값이 있는지 확인하고 
+	 있다면 Yes, 없다면 No를 출력하라	
+	*/
+	
+	public static int binarySearch(int []arr1, int target, int start, int end) {
+		int num = -1;
+		while(start <= end) {
+			int index = (start+end)/2;
+			int mid = arr1[index];
+			if(target == mid) num = mid;
+			if(target > mid) start = index+1;
+			else end = index-1;
+		}
+		return num;
+	}
+	
+	public static void main(String[] args) {
+        int [] arr1 = {8,3,7,9,2};
+        int [] arr2 = {5,7,9};
+        
+        for (int i = 0; i < arr2.length; i++) {
+			int num = binarySearch(arr1, arr2[i], 0, arr1.length-1);
+			if(num == -1) System.out.println("No");
+			else System.out.println("Yes");
+		}
+    }
+}
