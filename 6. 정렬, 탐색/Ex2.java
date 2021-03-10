6de32b382f234ec79196f0eaf93fa694eddbcb98
sun <78401555+AkkeSun@users.noreply.github.com>
2021-03-10 09:37:42 +0900

    Update Ex2.java

--- 6. 정렬, 탐색/Ex2.java
+++ 6. 정렬, 탐색/Ex2.java
@@ -4,16 +4,17 @@ public class Practice {
 	
 	// 이진탐색 : 정렬이 선행되어야 한다. 시작점 끝점 중간점을 두고 중간점과 비교해가며 데이터를 찾는다.	
     public static int binarySearch(int[] arr, int target, int start, int end) {
+	int result = -1;
         while (start <= end) {
             int mid = (start + end) / 2;
             // 찾은 경우 중간점 인덱스 반환
-            if (arr[mid] == target) return mid;
+            if (arr[mid] == target) result=mid;
             // 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
             else if (arr[mid] > target) end = mid - 1;
             // 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
             else start = mid + 1; 
         }
-        return -1;
+        return result;
     }
 
     public static void main(String[] args) {
